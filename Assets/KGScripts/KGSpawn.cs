514c16fcf3820c86708b1ee0549a68c9d4d5b19e
Kai <1021404@student.sae.edu.au>
2022-07-14 17:49:54 +0800

    odd one out greybox

---
+++ Assets/KGScripts/KGSpawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KGSpawn : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public GameObject[] things;
+   
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int randomNum = Random.Range(0,12);
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            int j = i + randomNum;
+            if (j >= spawnPoints.Length)
+            {
+                j -= spawnPoints.Length;
+            }
+            things[j].transform.position= spawnPoints[i].transform.position;
+                  
+        }
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
