87ebf8fccbb6474970ceebcf0a573f842a25da7f
indiejobbs <indiejobbs@gmail.com>
2022-07-24 23:07:02 +0800

    Games 99.99 % Done

--- Assets/SpotTheDifferenceController.cs
+++ Assets/SpotTheDifferenceController.cs
@@ -7,23 +7,63 @@ public class SpotTheDifferenceController : MonoBehaviour
 {
     public GameObject[] spotting;
     public Color[] cols;
+    Color getCol;
     int notIn;
+    float[] xSpeed;
+    float[] ySpeed;
     // Start is called before the first frame update
     void Start()
     {
-        var i = 0;
-        notIn = (Random.Range(0, cols.Length));
-        foreach(GameObject box in spotting) {
-            box.GetComponent<SpriteRenderer>().color = cols[i];
-            i ++;
-            if (i == notIn) i++;
-            if (i == 9) i = 0;
+        xSpeed = new float[spotting.Length];
+        ySpeed = new float[spotting.Length];
+
+        // Set speed values per instance
+        for(var i = 0; i < xSpeed.Length; i++) {
+            xSpeed[i] = Random.Range(-0.01f, 0.01f);
+            ySpeed[i] = Random.Range(-0.01f, 0.01f);
         }
+
+        // Set colour calues per instance and identify odd one out
+        var selected = Random.Range(0, spotting.Length);
+        notIn = Random.Range(0, cols.Length);
+        var buffer = 0;
+        for(var z = 0; z < spotting.Length; z++){
+            if (z != selected) {
+                spotting[z].GetComponent<Image>().color = cols[buffer];
+            } 
+            else {spotting[z].GetComponent<Image>().color = cols[notIn];}
+            if (buffer > 7) {buffer = 0;} else {buffer ++;}
+            if (buffer == notIn) {
+                if (buffer + 1 < 8) {buffer ++;} else {buffer = 0;}
+            }
+        }
+        getCol = spotting[selected].GetComponent<Image>().color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(var i = 0; i < spotting.Length; i++) {
+            spotting[i].transform.Translate(xSpeed[i], ySpeed[i], 0);
+            if (spotting[i].transform.position.x >= 8.5f) {
+                xSpeed[i] = -xSpeed[i];
+            } else if (spotting[i].transform.position.x <= -8.5f) {
+                xSpeed[i] = -xSpeed[i];
+            }
+
+            if (spotting[i].transform.position.y >= 5f) {
+                ySpeed[i] = -ySpeed[i];
+            } else if (spotting[i].transform.position.y <= -5f) {
+                ySpeed[i] = -ySpeed[i];
+            }
+        }
+    }
+
+    public void SpotMe(){
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Image>().color == getCol) {
+            {Debug.Log("You Win!");} // Win State
+        } else {
+            {Debug.Log("You Win!");} // Lose State
+        }
     }
 }
