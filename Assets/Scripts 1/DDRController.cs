363f9a17d96f60f0d1b406480774d3c360b9101b
indiejobbs <indiejobbs@gmail.com>
2022-07-28 12:45:02 +0800

    Added DDR code

--- Assets/Scripts 1/DDRController.cs
+++ Assets/Scripts 1/DDRController.cs
@@ -11,18 +11,27 @@ public class DDRController : MonoBehaviour
     int beatLimit = 16;
     int beatShape;
     int counter = -1;
+    int bpm = 120;
+    int note = 2;
+    float bps;
+    int trackBeat = 0;
+    bool canBeat = false;
+    int keepCount = 1;
+    int time = 0;
     float speed;
+    int dist = 15;
     Vector3 startPoint;
     // Start is called before the first frame update
     void Start()
     {
+        bps = bpm / note;
         arrowSequence = new GameObject[beatLimit];
         for(var i = 0; i < beatLimit; i++) {
             beatShape = Random.Range(0, 4);
-            startPoint = new Vector3(markers[beatShape].position.x + 15, markers[beatShape].position.y, 0);
+            startPoint = new Vector3(markers[beatShape].position.x + dist, markers[beatShape].position.y, 0);
             arrowSequence[i] = Instantiate(arrow, startPoint, Quaternion.identity);
         }
-        speed = Mathf.Abs((startPoint.x - markers[0].position.x) / (60 * 4));
+        speed = dist / (bps * 2);
         StartCoroutine(BeatTimer());
     }
 
@@ -31,11 +40,20 @@ public class DDRController : MonoBehaviour
     {
         for (var i = 0; i < beatLimit; i ++) {
             if (counter >= i) {
-                arrowSequence[i].transform.Translate(-speed, 0, 0);
+                arrowSequence[i].transform.position += new Vector3(-speed, 0, 0);
             }
         }
 
+        // Track Rhythm
+        time ++;
+        if (time/60 >= 0 + (trackBeat * bps)) {
+            canBeat = true;
+            trackBeat ++;
+            // if keepCount != 16 keepCount++ else keepCount = 1;
+        } else canBeat = false;
         
+        // Start On Beat
+        // if keepCouint == 1 moveArrow
     }
 
     IEnumerator BeatTimer() {
