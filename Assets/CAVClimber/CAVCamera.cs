ef9335453752d7294560d837c5a8eb689a78ae55
CodyAqV <1027166@student.sae.edu.au>
2022-07-28 14:19:27 +0800

    for real v2

---
+++ Assets/CAVClimber/CAVCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAVCamera : MonoBehaviour
+{
+
+    [SerializeField] Transform Player;
+    float offset = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, Player.position.y+offset, 0.01f),transform.position.z);
+        temp.y = Mathf.Clamp(temp.y, -20, 20);
+        transform.position = temp;
+    }
+}
