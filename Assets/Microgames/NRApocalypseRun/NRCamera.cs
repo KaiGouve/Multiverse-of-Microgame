9208e3b33140f14adab80cdf6f6a8d5b9b174a75
Kai <1021404@student.sae.edu.au>
2022-08-11 19:14:36 +0800

    Adding Stuff

---
+++ Assets/Microgames/NRApocalypseRun/NRCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NRCamera : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.active){
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x,player.transform.position.x,0.1f),transform.position.y,transform.position.z);
+        }
+        
+    }
+}
