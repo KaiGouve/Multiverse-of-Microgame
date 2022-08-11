fe912ff3bc9d16d0187820b4f2905bddd41c25f4
CodyAqV <1027166@student.sae.edu.au>
2022-08-11 17:21:18 +0800

    DOG DONE :D

---
+++ Assets/Global Scenes/MainScene/Timer/ControlsAnim.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsAnim : MonoBehaviour
+{
+    [SerializeField] Sprite[] Images;
+    int number;
+    [SerializeField] float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("fuckYou");
+    }
+
+    IEnumerator fuckYou()
+    {
+        yield return new WaitForSeconds(delay);
+        number++;
+        number = number==Images.Length? 0:number;
+        GetComponent<Image>().sprite = Images[number];
+        StartCoroutine("fuckYou");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
